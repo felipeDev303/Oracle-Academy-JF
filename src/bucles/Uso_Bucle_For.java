b37d62943da386991e9281e4670f2a373a9f0fa8
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-09-12 19:41:02 -0300

    Add Uso_Bucle_For Class

---
+++ src/bucles/Uso_Bucle_For.java
@@ -0,0 +1,9 @@
+package bucles;
+
+public class Uso_Bucle_For {
+    public static void main(String[] args){
+        for(int i=0;i<10;i++){
+            System.out.println("No debo instigar a la revolución.");
+        }
+    }
+}
