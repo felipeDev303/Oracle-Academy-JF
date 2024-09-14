4c7d282d792d089ede8756a5fc73e3b5db391485
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-09-14 00:36:50 -0300

    fix calculator

--- src/bucles/CalculatorBasicExample.java
+++ src/bucles/CalculatorBasicExample.java
@@ -4,26 +4,44 @@ import java.util.Scanner;
 
 public class CalculatorBasicExample{
     public static void main(String[] args){
+
         boolean quit = false;
         int num1 = 10, num2 = 6, answer = 0;
-        char operand = '';
+        char operand = '\0';
+
         Scanner in = new Scanner(System.in);
+
         do {
-            System.out.println("Please enter a mathematical operand ");
+            System.out.println("Please enter a mathematical operand (*,+,-,/): ");
             String input = in.next();
-            char operand = input.charAt(0);
+            operand = input.charAt(0);
             switch(operand) {
                 case '*': answer = num1 * num2;
                     break;
                 case '+': answer = num1 + num2;
                     break;
-                case '-': answer = num1 – num2;
+                case '-': answer = num1 - num2;
+                    break;
                 case '/': answer = num1 / num2;
-                default: System.out.println("Invalid input.");
+                    if(num2!=0){
+                        answer = num1 / num2;
+                    }else{
+                        System.out.println("No se puede dividir por 0");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid input.");
+                    continue; // Salta la impresión de la respuesta si el input es inválido
             }
+            System.out.println("El resultado es: " + answer);
+
             System.out.println("Quit? Y/N");
-            if(in.next().equalsIgnorecase("Y"))
+            if(in.next().equalsIgnoreCase("Y")){
                 quit=true;
+            }
         } while(!quit);
+
+        in.close();
     }//end method main
+
 }//end class Section5Example
