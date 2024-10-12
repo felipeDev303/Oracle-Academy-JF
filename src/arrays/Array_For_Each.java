6d1262cfa4ae73b2908e7cb5cea88f0148cd4ac9
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-10-11 21:48:44 -0300

    Conceptos fundamentales de Java JF 6-1: Arrays For Each Oracle Academy

---
+++ src/arrays/Array_For_Each.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Array_For_Each {
+    public static void main (String[] args){
+
+        // Declaración e inicialización estática del array
+        String [] paises={"España","Chile","Argentina","Colombia","México","Perú","Ecuador","Venezuela"};
+
+        // For-each
+        for(String i:paises){
+            System.out.println(i);
+        }
+    }
+}
