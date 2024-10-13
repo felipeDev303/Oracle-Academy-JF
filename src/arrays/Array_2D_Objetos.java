d24e831f51d8d8ca38d8cba1706d9b351ecee4df
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-10-12 21:20:38 -0300

    Conceptos fundamentales de Java JF 6-1: Arrays bidimensional con objetos y for-each Oracle Academy

--- src/arrays/Array_2D_Objetos.java
+++ src/arrays/Array_2D_Objetos.java
@@ -3,17 +3,21 @@ package arrays;
 public class Array_2D_Objetos {
     public static void main(String[] args){
         //Declaración e inicialización de array bidimensional
-        String[][] flower={
-                {"Rosas","Blancas","Rojas","Rosadas"},
-                {"Girasoles","Blancas","Rojas","Amarillas"},
-                {"Tulipanes","Blancas","Violeta","Amarillas"}
+        String[][] beer={
+                {"Tamango","Cortacorriente","Pantera","Supersonic"},
+                {"Jester","Cosmonauta","Resinosa","Hopyjuz"},
+                {"Granizo","Gran Ipa","Bomba 6","Orquídea"}
         };
 
-        for(String[]fila:flower){
-            System.out.println();
-            for(String columna:fila){
-                System.out.println(columna + " ");
+        // Recorrer array bidimensional usando for-each
+        for(String[] beerType:beer){
+            // Imprimir el primer elemento de cada array (Marca de cerveza)
+            System.out.println("Marca de cerveza artesanal: " + beerType[0]);
+            System.out.println("Tipos disponibles:");
+            for(int i=1;i < beerType.length;i++){
+                System.out.println("- " + beerType[i]);
             }
+            System.out.println(); //Separar las cervezas con un espacio en blanco
         }
     }
 }
