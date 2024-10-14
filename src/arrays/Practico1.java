3e082fe538eee40186e8553fc3ce53e99ee20a5d
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-10-14 00:31:34 -0300

    Conceptos fundamentales de Java JF 6-1: Práctico en Java Oracle Academy

---
+++ src/arrays/Practico1.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class Practico1 {
+    public static void main (String[] args) {
+        /* //1. Declare una calificación de nombre de array de una
+        dimensión del tipo int que pueda contener 9 valores.
+        int[]calification=new int[9];*/
+
+        /*// 2. Declare un precio con nombre de array de 2 dimensiones de
+        tipo float que tenga 10 filas y 3 columnas.
+        float[][] precio = new float[3][10];*/
+
+        /*3. Declare e inicialice un array de 2 dimensiones denominado arreglo
+        de un tipo largo que tenga 4 filas y 3 columnas para que
+        todos los valores estén configurados en 5.
+        long[][]arreglo={
+                {5L,5L,5L},
+                {5L,5L,5L},
+                {5L,5L,5L},
+                {5L,5L,5L}
+        };*/
+        /*4. Declare e inicialice un array de bytes de una dimensión denominado
+        valores de tamaño 10 de manera que todas las entradas contengan 1.*/
+        byte[]valores= {1,1,1,1,1,1,1,1,1,1};
+    }
+}
---
+++ src/arrays/Practico2.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class Practico2 {
+    public static void main(String[] args){
+        int num[] = {7,7,6,6,5,5,4,4};
+        for(int i = 0; i < 8; i = i + 2) {
+            System.out.print(num[i]);
+        }
+    }
+}
