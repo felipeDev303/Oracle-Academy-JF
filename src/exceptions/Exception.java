e7536dc1aa8e512aa910f1d5f8301f83968a32b4
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-11-12 12:02:48 -0300

    add Exception

---
+++ src/exceptions/Exception.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class Exception {
+    public static void main(String[]args){
+        int[] example = {1, 2, 3};
+        System.out.println(example[3]);
+    }
+} // Index out of bounds exception
