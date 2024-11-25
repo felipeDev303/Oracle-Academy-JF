2e7a77e0888d3be8a30bde7f563de4ee657e8999
felipeDev303 <felipe.morales.roa@ciisa.cl>
2024-11-25 02:18:34 -0300

    Herencia - Jefatura class

--- src/poo/Uso_Empleado.java
+++ src/poo/Uso_Empleado.java
@@ -6,38 +6,23 @@ import java.util.*;
 public class Uso_Empleado {
     public static void main(String[]args){
 
-        /*// Creamos 3 instancias (objetos) de la clase Empleado
-        Empleado empleado1=new Empleado("Felipe Morales",500000,2020,12,11);
-        Empleado empleado2=new Empleado("Papu Gomez",400000,2021,11,10);
-        Empleado empleado3=new Empleado("Lionel Messi",600000,2017,9,8);
-
-        // Utilizamos el método sube sueldo
-        empleado1.subeSueldo(5);
-        empleado2.subeSueldo(5);
-        empleado3.subeSueldo(5);
-
-        System.out.println("Nombre: " + empleado1.getNombre() + " ,su nuevo sueldo es "+ empleado1.getSueldo()
-        + " Fecha de contrato: " + empleado1.getFechaContrato());
-
-        System.out.println("Nombre: " + empleado2.getNombre() + " ,su nuevo sueldo es "+ empleado2.getSueldo()
-                + " Fecha de contrato: " + empleado2.getFechaContrato());
-
-        System.out.println("Nombre: " + empleado3.getNombre() + " ,su nuevo sueldo es "+ empleado3.getSueldo()
-                + " Fecha de contrato: " + empleado3.getFechaContrato());*/
-
         // Versión con arrays
-        Empleado[] misEmpleados=new Empleado[3];
+
+        Jefatura jefe_RRHH = new Jefatura("Don Carter",1000000,2023,1,2);
+
+        jefe_RRHH.setIncentivo(25000);
+
+        Empleado[] misEmpleados=new Empleado[5];
         misEmpleados[0]=new Empleado("Felipe Morales",500000,2020,12,11);
         misEmpleados[1]=new Empleado("Papu Gomez",400000,2021,11,10);
         misEmpleados[2]=new Empleado("Lionel Messi",600000,2017,9,8);
+        misEmpleados[3]=jefe_RRHH; // Polimorfismo en acción (Podemos utilizar un objeto de la subclase cuando
+        // espero un objeto de la superclase)
+        misEmpleados[4]=new Jefatura("Tío Rene",1000000,2023,3,1);
 
-        /*for(int i=0;i<misEmpleados.length;i++){
-            misEmpleados[i].subeSueldo(5);
-        }
+        Jefatura jefe_MKT = (Jefatura) misEmpleados[4];
 
-        for(int i=0;i<misEmpleados.length;i++){
-            System.out.println("Nombre: " + misEmpleados[i].getNombre() + " Sueldo: "+ misEmpleados[i].getSueldo() + " Fecha Contrato: "+misEmpleados[i].getFechaContrato());
-        }*/
+        jefe_MKT.setIncentivo(35000);
 
         // Versión for mejorado
         for(Empleado e: misEmpleados){
@@ -51,43 +36,43 @@ public class Uso_Empleado {
 }
 
 // Clase que construye el objeto
-class Empleado{
+class Empleado {
 
     // Constructor (public para que sea accesible de otras clases)
-    public Empleado(String nom, double sue, int agno, int mes, int dia){
+    public Empleado(String nom, double sue, int agno, int mes, int dia) {
 
         // Variables
-        nombre=nom;
-        sueldo=sue;
+        nombre = nom;
+        sueldo = sue;
         // Nombre de la clase / Nombre del objeto / new (llamar al constructor de la clase) / elegimos constructor(fecha)
-        GregorianCalendar calendario= new GregorianCalendar(agno,mes-1,dia);
+        GregorianCalendar calendario = new GregorianCalendar(agno, mes - 1, dia);
 
         // En la variable altaContrato almacenamos lo que nos retorne el método del objeto calendario
-        altaContrato=calendario.getTime();
+        altaContrato = calendario.getTime();
     }
 
     // Sobrecarga de constructores
-    public Empleado (String nom){
-        this(nom,30000,2000,1,1);
+    public Empleado(String nom) {
+        this(nom, 30000, 2000, 1, 1);
     }
 
     //Getter
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
 
-    public double getSueldo(){
+    public double getSueldo() {
         return sueldo;
     }
 
-    public Date getFechaContrato(){
+    public Date getFechaContrato() {
         return altaContrato;
     }
 
     // Setter
-    public void subeSueldo(double porcentaje){
-        double aumento=sueldo*porcentaje/100;
-        sueldo +=aumento;
+    public void subeSueldo(double porcentaje) {
+        double aumento = sueldo * porcentaje / 100;
+        sueldo += aumento;
     }
 
     // Variables de clase (propiedades del objeto)
@@ -95,3 +80,36 @@ class Empleado{
     private double sueldo;
     private Date altaContrato;
 }
+
+class Jefatura extends Empleado {
+
+    // Constructor de la clase hija
+    public Jefatura(String nom, double sue, int agno, int mes, int dia) {
+
+        // Constructor de la clase padre
+        super(nom, sue, agno, mes, dia);
+
+    }
+
+    // Método setter
+    public void setIncentivo(double b) {
+        incentivo = b;
+    }
+
+    // Método getter
+    public double getSueldo() {
+        double sueldoJefe = super.getSueldo();
+        return sueldoJefe + incentivo;
+    }
+
+    // variable de clase
+    private double incentivo;
+}
+
+    class Director extends Jefatura{
+        public Director(String nom, double sue, int agno, int mes, int dia) {
+
+            // Constructor de la clase padre
+            super(nom, sue, agno, mes, dia);
+        }
+    }
